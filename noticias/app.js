071d7f8b85f4fd932df452f6044f400dca90659a
aluno <edson.shisui@gmail.com>
2018-03-05 08:00:48 -0400

    ejs instalado

--- noticias/app.js
+++ noticias/app.js
@@ -1,6 +1,23 @@
 var express = require ('express');
 var app = express();
 
+
+app.set('view engine', 'ejs');
+
+
+app.get('/cursos', function(req,res){
+	res.render("secao/cursos");
+});
+
+app.get('/esportes', function(req,res){
+	res.render("secao/esportes");
+});
+
+app.get('/pesquisa', function(req,res){
+	res.render("secao/pesquisa");
+});
+
+
 app.listen(3000, function(){
-	console.log("WServidor rodando com Express");
+	console.log("Servidor rodando com Express");
 });
