e3db40b2ee92821a53c965ddb7fb479be3d78057
aluno <edson.shisui@gmail.com>
2018-03-06 08:25:15 -0400

    Nodemon

--- noticias/app.js
+++ noticias/app.js
@@ -1,9 +1,4 @@
-var express = require ('express');
-var app = express();
-
-
-app.set('view engine', 'ejs');
-
+var app = require('./config/server');
 
 app.get('/', function(req,res){
 	res.render("home/index");
@@ -19,5 +14,5 @@ app.get('/noticias', function(req,res){
 
 
 app.listen(3000, function(){
-	console.log("Servidor rodando com Express");
+	console.log("Server status: ON");
 });
